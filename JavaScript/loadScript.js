5d6d9611a0d5895ebaca24c2825bcb63fe16fd00
猪股一歩希 <bobby@MacBook-Pro-3.local>
2020-07-29 20:26:03 +0900

    heroku

--- JavaScript/loadScript.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const targetUrl = `./JavaScript/model.js?p=${(new Date()).getTime()}`;
-    const scriptElement = document.createElement('script');
-    scriptElement.src = targetUrl;
-    document.head.appendChild(scriptElement);
-});
